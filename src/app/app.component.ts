3b3c59a047c66210d4e3aeb4b1b1242680ba471b
SLH018 <slh018@yahoo.co.uk>
2023-09-12 19:53:14 +0100

    Updated to use title property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'football';
+  title = 'football updates';
 }
