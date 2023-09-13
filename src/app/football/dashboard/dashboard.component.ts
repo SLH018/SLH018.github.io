f272205a9af999f695aebcf2f1553923082dc249
SLH018 <slh018@yahoo.co.uk>
2023-09-13 20:50:37 +0100

    Updated to integrate league standings component.

--- src/app/football/dashboard/dashboard.component.ts
+++ src/app/football/dashboard/dashboard.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../button/button.component';
+import { LeagueStandingsComponent } from '../league-standings/league-standings.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, LeagueStandingsComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
   public listOfCountries: string[] = ['England', 'Spain', 'Germany', 'France', 'Italy'];
+  public selectedCountry?: string;
 
-  public callService(country: string){
-    console.log('The button', country, ' was clicked')
+  public onSelect(country: string){
+    console.log('The button', country, ' was clicked');
+    this.selectedCountry = country;
   }
 }
--- src/app/football/league-standings/league-standings.component.ts
+++ src/app/football/league-standings/league-standings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,6 +9,8 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./league-standings.component.scss'],
 })
 export class LeagueStandingsComponent {
+
+  @Input() country?: string;
 
   
   
