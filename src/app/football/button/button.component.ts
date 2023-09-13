1ccee133788580f067393e15d5c0abae88be2ceb
SLH018 <slh018@yahoo.co.uk>
2023-09-13 20:30:30 +0100

    Updated to add Angular Material

--- src/app/football/button/button.component.ts
+++ src/app/football/button/button.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
---
+++ src/app/football/league-standings/league-standings.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-league-standings',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './league-standings.component.html',
+  styleUrls: ['./league-standings.component.scss'],
+})
+export class LeagueStandingsComponent {
+
+  
+  
+}
